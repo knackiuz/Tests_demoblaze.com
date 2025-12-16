e2198c09c806f337c6c91a00c17b406e80ca96bb
knackiuz <knackiuz@gmail.com>
2025-12-16 22:50:16 +0200

    added comment to test 'shopThreeItemsFromDifferentCategories' !not finished!

--- src/test/java/tests/ShopTest.java
+++ src/test/java/tests/ShopTest.java
@@ -25,36 +25,36 @@ public class ShopTest {
         String laptopName = "Sony vaio i5";
         String monitorName = "Apple monitor 24";
         String phoneName = "Nexus 6";
-        new HomePage().openPage().
-                verifyPageIsLoaded()
-                .selectCategory("Laptops")
-                .checkProductCard(laptopName)
-                .productClick(laptopName)
-                .verifyPageIsLoaded()
-                .checkProductName(laptopName)
-                .clickAddToCart()
-                .homeClick()
-                .verifyPageIsLoaded()
-                .selectCategory("Monitors")
-                .checkProductCard(monitorName)
-                .productClick(monitorName)
-                .verifyPageIsLoaded()
-                .checkProductName(monitorName)
-                .clickAddToCart()
-                .homeClick()
-                .verifyPageIsLoaded()
-                .selectCategory("Phones")
-                .checkProductCard(phoneName)
-                .productClick(phoneName)
-                .verifyPageIsLoaded()
-                .checkProductName(phoneName)
-                .clickAddToCart()
-                .cartClick()
-                .verifyPageIsLoaded()
-                .checkCartItems(3)
-                .checkProductName(laptopName)
-                .checkProductName(monitorName)
-                .checkProductName(phoneName);
+        new HomePage().openPage().                                  //Open Home page
+                verifyPageIsLoaded()                                //The page is loaded
+                .selectCategory("Laptops")              //Select category 'Laptops'
+                .checkProductCard(laptopName)                       //Check that the product is available
+                .productClick(laptopName)                           //Click on the product
+                .verifyPageIsLoaded()                               //Verify that product page is loaded
+                .checkProductName(laptopName)                       //Check the product's name
+                .clickAddToCart()                                   //Click button 'Add to Cart'
+                .homeClick()                                        //Click 'Home' in the navigation
+                .verifyPageIsLoaded()                               //Verify that Home page is loaded
+                .selectCategory("Monitors")             //Select category 'Monitors'
+                .checkProductCard(monitorName)                      //Check that the product is available
+                .productClick(monitorName)                          //Click on the product
+                .verifyPageIsLoaded()                               //Verify that product page is loaded
+                .checkProductName(monitorName)                      //Check the product's name
+                .clickAddToCart()                                   //Click button 'Add to Cart'
+                .homeClick()                                        //Click 'Home' in the navigation
+                .verifyPageIsLoaded()                               //Verify that Home page is loaded
+                .selectCategory("Phones")               //Select category 'Phones'
+                .checkProductCard(phoneName)                        //Check that the product is available
+                .productClick(phoneName)                            //Click on the product
+                .verifyPageIsLoaded()                               //Verify that product page is loaded
+                .checkProductName(phoneName)                        //Check the product's name
+                .clickAddToCart()                                   //Click button 'Add to Cart'
+                .cartClick()                                        //Go to Cart page
+                .verifyPageIsLoaded()                               //Verify that cart page is loaded
+                .checkCartItems(3)                                  //Check that all 3 products in the cart
+                .checkProductName(laptopName)                       //Check Laptop product's name
+                .checkProductName(monitorName)                      //Check Monitor product's name
+                .checkProductName(phoneName);                       //Check Phone product's name
 
     }
 }
