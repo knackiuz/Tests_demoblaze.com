8d6741b0883382729a61700622eb931497b2b1b4
knackiuz <knackiuz@gmail.com>
2026-01-21 11:45:24 +0200

    added Allure listener

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,8 @@
 package base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 //Base class for all tests
@@ -18,5 +20,15 @@ public class BaseTest {
 
         //Set to true to prevent the browser from closing after execution
         Configuration.holdBrowserOpen = false;
+    }
+
+    @BeforeAll
+    static void setupAllureReports() {
+        // This listener attaches screenshots and steps to the Allure report
+        // Adding English comments as per your instructions
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)      // Save screenshots automatically on failure
+                .savePageSource(false)  // Do not save heavy HTML source to keep report light
+        );
     }
 }
